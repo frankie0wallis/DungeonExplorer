5423a40d9653f134b9ffe77ebb311e1e37371271
Frankie Wallis <frankiewallis777@hotmail.com>
2025-03-10 19:22:54 +0000

    Created Monster.cs
    Created list of Monster with health and attack attributes, also created a defence function

---
+++ Monster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    class Monster
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        private static Random random = new Random();
+        private static List<Monster> possibleMonsters = new List<Monster>
+        {
+            new Monster("Goblin", 10, 5),
+            new Monster("Orc", 20, 10),
+            new Monster("Troll", 30, 15),
+            new Monster("Dragon", 50, 20),
+            new Monster("Beholder", 40, 25),
+            new Monster("Lich", 60, 30),
+            new Monster("Balrog", 70, 35),
+            new Monster("Chimera", 80, 40),
+            new Monster("Hydra", 90, 45),
+            new Monster("Kraken", 100, 50),
+        };
+        public Monster(string name, int health, int attack)
+        {
+            Name = name;
+            Health = health;
+            Attack = attack;
+        }
+        public void AttackPlayer(Player player)
+        {
+            int damage = Attack - player.Defence;
+            if (damage < 0)
+            {
+                damage = 0;
+            }
+            player.Health -= damage;
+            Console.WriteLine(Name + " attacked you for " + damage + " damage!");
+        }
+    }
+}
