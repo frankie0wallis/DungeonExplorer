a86ce42c627280c123fac8c7009a1aa829cb6751
Frankie Wallis <frankiewallis777@hotmail.com>
2025-03-10 21:41:10 +0000

    Update to Player.cs
    Improved Inventory function with access to Weapons and Potions and framework for Armour

--- Player.cs
+++ Player.cs
@@ -1,25 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DungeonExplorer
 {
     public class Player
     {
         public string Name { get; private set; }
-        public int Health { get; private set; }
-        private List<string> inventory = new List<string>();
+        public int Health { get; private set; } = 100;
+        public List<Weapon> Inventory { get; private set; } = new List<Weapon>();
+        public List<Item> Items { get; private set; } = new List<Item>();
 
-        public Player(string name, int health) 
+        public void PickUpWeapon(Weapon weapon)
         {
-            Name = name;
-            Health = health;
+            Inventory.Add(weapon);
+            Console.WriteLine($"You found an item: {weapon.Name} (Attack Power: {weapon.AttackPower})");
         }
-        public void PickUpItem(string item)
-        {
 
-        }
-        public string InventoryContents()
+        public string ShowInventory()
         {
-            return string.Join(", ", inventory);
+            Console.WriteLine("Inventory:");
+            foreach (var weapon in Inventory)
+                Console.WriteLine($"- {weapon.Name} (Attack: {weapon.AttackPower})");
+            foreach (var potion in Item)
+                Console.WriteLine($"- {potion.Name} (Heals: {potion.HealingAmount})");
+            if (EquippedArmour != null)
+                Console.WriteLine($"- Equipped Armour: {EquippedArmour}");
         }
     }
 }
