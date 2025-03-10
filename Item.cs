ec5963fbe72958e0f4066ed3381a10c65f4367ff
Frankie Wallis <frankiewallis777@hotmail.com>
2025-03-10 20:28:20 +0000

    Recreated Item.cs
    Added a way to heal via potions implemented in the same fashion of Weapons/Armour.

--- Item.cs
+++ Item.cs
@@ -1,40 +1,35 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DungeonExplorer
 {
     public class Item
     {
-        public string Name { get; private set; }
-        public int AttackPower { get; private set; }
+        public string Name { get; set; }
+        public int HealingAmount { get; set; }
         private static Random random = new Random();
-        public Item(string name, int attackpower)
+
+        private static List<Item> possibleitems = new List<Item>
+        {
+            new Item("Small Health Potion", 10),
+            new Item("Medium Health Potion", 20),
+            new Item("Large Health Potion", 30),
+            new Item("Legendary Health Potion", 50),
+            new Item("Revive Scroll", 100),
+        };
+
+        public Item(string name, int healingamount)
         {
             Name = name;
-            AttackPower = attackpower;
+            HealingAmount = healingamount;
         }
-        private static List<Item> possibleItems = new List<Item>
-        {
-            new Item("Sword", 15),
-            new Item("Axe", 15),
-            new Item("Bow", 20),
-            new Item("Dagger", 10),
-            new Item("Staff", 25),
-            new Item("Mace", 30),
-            new Item("Spear", 35),
-            new Item("Crossbow", 35),
-            new Item("Halberd", 40),
-            new Item("Scythe", 25),
-            new Item("Flail", 30),
-            new Item("Katana", 50),
-            new Item("Boomerang", 5),
-            new Item("Shuriken", 10),
-            new Item("Kunai", 10),
-            new Item("Trident of Poseidon", 75),
-        };
+
         public static Item GetRandomItem()
         {
-            return possibleItems[random.Next(possibleItems.Count)];
+            return possibleitems[random.Next(possibleitems.Count)];
         }
     }
 }
