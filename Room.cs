07b8602b05584a5fbbf9dd4ff6bfd62605cc7d53
Frankie Wallis <frankiewallis777@hotmail.com>
2025-03-10 19:08:24 +0000

    Added Random Events for Rooms

--- Room.cs
+++ Room.cs
@@ -1,8 +1,11 @@
-﻿namespace DungeonExplorer
+﻿using System;
+
+namespace DungeonExplorer
 {
     public class Room
     {
         private string description;
+        private static Random random = new Random();
 
         public Room(string description)
         {
@@ -12,6 +15,29 @@
         public string GetDescription()
         {
             return description;
+        }
+
+        public void Enter(Player player)
+        {
+            int eventRoll = random.Next(1, 5);
+            switch (eventRoll)
+            {
+                case 1:
+                    Console.WriteLine("You enter the room and find a chest.");
+                    player.PickUpItem(Item.GetRandomItem());
+                    break;
+                case 2:
+                    Console.WriteLine("You enter the room and find a monster.");
+                    player.Attack();
+                    break;
+                case 3:
+                    Console.WriteLine("You enter the room and find a trap.");
+                    player.TakeDamage(10);
+                    break;
+                case 4:
+                    Console.WriteLine("You enter the room and find nothing.");
+                    break;
+            }
         }
     }
 }
