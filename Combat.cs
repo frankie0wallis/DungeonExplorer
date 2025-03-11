2dfb1013c2f27a47b75bee7f2b7bd0dc5863bd03
Frankie Wallis <frankiewallis777@hotmail.com>
2025-03-11 14:27:17 +0000

    Update to Combat.cs
    At options for the player to choose from.

--- Combat.cs
+++ Combat.cs
@@ -15,12 +15,38 @@ namespace DungeonExplorer
             while (player.Health > 0 && monster.Health > 0)
             {
                 Console.WriteLine("(a) Attack, (i) Check Inventory, (p) Use Item, (r) Run Away");
-                string input = Console.ReadLine()?.ToLower;
+                string input = Console.ReadLine()?.ToLower();
 
-                if (Action == "a")
+                if (input == "a")
                 {
                     Weapon weapon = player.GetBestWeapon();
                     monster.Health -= weapon.AttackPower;
                     Console.WriteLine($"You attacked the {monster.Name} with {weapon.Name} for {weapon.AttackPower} damage! It has {monster.Health} health left!");
+
+                    if (monster.Health <= 0)
+                    {
+                        Console.WriteLine($"You defeated the {monster.Name}!");
+                        return;
+                    }
+                }
+                else if (input == "i")
+                {
+                    player.ShowInventory();
+                }
+                else if (input == "p")
+                {
+                    player.UseItem();
+                }
+                else if (input == "r")
+                {
+                    Console.WriteLine("You ran away!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid action!");
                 }
             }
+        }
+    }
+}
